138192ca443bc4773a47d350f46ebe193f82ed84
Desirée <desireekonectia@gmail.com>
2026-03-11 19:37:21 +0100

    ...

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.secret_key = os.getenv("SECRET_KEY", "clave_segura_123")
 def conectar_bd(): # Nombre simplificado
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD")
